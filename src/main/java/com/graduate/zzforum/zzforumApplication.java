0d00b6aed4b0cb280ddfb61c246a8ae49147da45
Amadeus9029 <965720890@qq.com>
2020-02-06 23:54:05 +0800

    first commit

--- src/main/java/com/graduate/zzforum/zzforumApplication.java
+++ src/main/java/com/graduate/zzforum/zzforumApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZzforumApplication {
+public class zzforumApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ZzforumApplication.class, args);
+		SpringApplication.run(zzforumApplication.class, args);
 	}
 }
---
+++ src/test/java/com/graduate/zzformula/zzforumApplicationTests.java
@@ -0,0 +1,19 @@
+package com.graduate.zzformula;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class zzforumApplicationTests {
+
+	@Test
+	void contextLoads() {
+		
+	}
+
+}
